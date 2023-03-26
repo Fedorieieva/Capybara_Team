c13f41e0f79ee4a77bba99616ea7ca91761ecc1a
Fedorieieva Dariia <fedorieieva.dariia@lll.kpi.ua>
2023-03-26 17:11:53 +0300

    changed

--- tasks/improper_fraction_multiplication/improper_fraction_multiplication/main.cpp
+++ tasks/improper_fraction_multiplication/improper_fraction_multiplication/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 /*Design a class for representing an improper fraction,
 which is specified by a mixed number: the integer part,
@@ -18,60 +19,58 @@ standard output stream operator "<<").*/
 using namespace std;
 
 class Fraction {
-	int num;
-	int numerator;
-	int denomirator;
+	string fraction;
 
 	void check_input() {
-		if (denomirator == 0) {
+		int pos = fraction.find(":");
+		if (stoi(fraction.substr(pos + 1)) == 0) {
 			cout << "[Error] division by 0" << endl;
 			exit(1);
 		}
 	}
 
 public:
-	Fraction() {
-		num = 0;
-		numerator = 0;
-		denomirator = 0;
-	}
+	Fraction() { fraction = ""; }
 
-	Fraction(int n, int nume, int denomi) {
-		num = n;
-		numerator = nume;
-		denomirator = denomi;
+	Fraction(string str) {
+		fraction = str;
 		check_input();
 	}
 
 	Fraction operator *(Fraction& other) {
 		Fraction temp;
-		int n1, n2, nume;
-		n1 = (this->num * this->denomirator) + this->numerator;
-		n2 = (other.num * other.denomirator) + other.numerator;
-		nume = n1 * n2;
-		temp.denomirator = this->denomirator * other.denomirator;		
-		temp.num = nume / temp.denomirator;
-		temp.numerator = nume - (temp.num * temp.denomirator);
+
+		int pos1 = this->fraction.find(" ");
+		int num1 = stoi(this->fraction.substr(0, pos1));
+		int pos2 = this->fraction.find(":");
+		int numerator1 = stoi(this->fraction.substr(pos1 + 1, pos2));
+		int denomirator1 = stoi(this->fraction.substr(pos2 + 1));
+		
+		int n1 = (num1 * denomirator1) + numerator1;
+
+		pos1 = other.fraction.find(" ");
+		int num2 = stoi(other.fraction.substr(0, pos1));
+		pos2 = other.fraction.find(":");
+		int numerator2 = stoi(other.fraction.substr(pos1 + 1, pos2));
+		int denomirator2 = stoi(other.fraction.substr(pos2 + 1));
+
+		int n2 = (num2 * denomirator2) + numerator2;
+
+		int nume = n1 * n2;
+
+		int res_d = denomirator1 * denomirator2;
+		int res_num = nume / res_d;
+		int res_nume = nume - (res_num * res_d);
+
+		temp.fraction = to_string(res_num) + " " + to_string(res_nume) + ":" + to_string(res_d);
 		return temp;
 	}
 
 	friend ostream& operator <<(ostream& os, const Fraction& f) {
-		os << f.num << " " << f.numerator << ":" << f.denomirator;
+		os << f.fraction;
 		return os;
 	}
 };
-
-void input(int& n, int& nume, int& denomi) {
-	cout << endl << "Please enter the integer part of a improper fraction: ";
-	cout << endl << "integer = ";
-	cin >> n;
-	cout << "Please enter numerator for the fraction: " << endl;
-	cout << "numerator = ";
-	cin >> nume;
-	cout << "Please enter denominator for the fraction: " << endl;
-	cout << "denominator = ";
-	cin >> denomi;
-}
 
 void output(Fraction frac1, Fraction frac2) {
 	cout << endl << "Improper fraction 1 = " << frac1 << endl;
@@ -79,13 +78,17 @@ void output(Fraction frac1, Fraction frac2) {
 }
 
 int main() {
-	int n, nume, denomi;
+	string str;
 
-	input(n, nume, denomi);
-	Fraction frac1(n, nume, denomi);
+	cout << "Enter your fraction (example '2 3:7')" << endl;
+	cout << "Improper fraction 1 = ";
+	getline(cin, str);
+	Fraction frac1(str);
 
-	input(n, nume, denomi);
-	Fraction frac2(n, nume, denomi);
+	cout << endl << "Enter your fraction (example '2 3:7')" << endl;
+	cout << "Improper fraction 2 = ";
+	getline(cin, str);
+	Fraction frac2(str);
 
 	output(frac1, frac2);
 
